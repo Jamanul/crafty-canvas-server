1929f7850d3405d78067f7ca3264651de3870f6d
Jamanul <jk.skb.25@gmail.com>
2024-04-27 21:15:58 +0600

    all-arts post/create api server side section done

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/',(req,res)=>{
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dibths0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+//console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,6 +35,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("artDB");
+    const allArtCollection = database.collection("allArtCollection");
+    //allArt
+    app.post('/all-arts',async (req,res)=>{
+        const art = req.body;
+        const result =await allArtCollection.insertOne(art)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
