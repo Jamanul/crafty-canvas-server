8850b79e0695a55833bf45cd9575ff86cf09bee2
Jamanul <jk.skb.25@gmail.com>
2024-04-28 17:22:25 +0600

    delete  persoanl  data by email server side section done

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ async function run() {
         res.send(result)
     })
 
+    app.delete('/my-arts/:id',async(req,res)=>{
+      const id =req.params.id
+      const query ={_id:new ObjectId(id)}
+      const result =await allArtCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
